06bb6697d1832a541a706ccabc3e13030c78524a
adriii28 <adrigallego28@gmail.com>
2023-10-28 18:59:47 +0200

    Proyecto 2

---
+++ TA20Ej2_AdrianGallego/src/main/java/TA20Ej2_AdrianGallego/TA20Ej2_AdrianGallego/App.java
@@ -0,0 +1,9 @@
+package TA20Ej2_AdrianGallego.TA20Ej2_AdrianGallego;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		vistaEj2 window = new vistaEj2();
+		window.frame.setVisible(true);
+	}
+}
