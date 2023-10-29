60ed414ea2d074523663db94501eef10cc722b24
adriii28 <adrigallego28@gmail.com>
2023-10-29 19:31:47 +0100

    Ej 6

---
+++ TA20Ej6_AdrianGallego/src/main/java/TA20Ej6_AdrianGallego/TA20Ej6_AdrianGallego/App.java
@@ -0,0 +1,9 @@
+package TA20Ej6_AdrianGallego.TA20Ej6_AdrianGallego;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		vistaEj6 window = new vistaEj6();
+		window.frame.setVisible(true);
+	}
+}
