fda2427d983eef7a87bd2473f1b4e4089e7bd7c9
adriii28 <95284762+adriii28@users.noreply.github.com>
2023-10-29 20:29:52 +0100

    Delete TA20Ej4_AdrianGallego directory

--- TA20Ej4_AdrianGallego/src/main/java/TA20Ej4_AdrianGallego/TA20Ej4_AdrianGallego/App.java
+++
@@ -1,9 +0,0 @@
-package TA20Ej4_AdrianGallego.TA20Ej4_AdrianGallego;
-
-public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		vistaEj4 window = new vistaEj4();
-		window.frame.setVisible(true);
-	}
-}
