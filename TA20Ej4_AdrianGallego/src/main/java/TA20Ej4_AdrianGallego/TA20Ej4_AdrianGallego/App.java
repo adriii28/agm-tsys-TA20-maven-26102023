002528a56e8417e8647133cc114475e264ebb295
adriii28 <adrigallego28@gmail.com>
2023-10-29 19:30:53 +0100

    Ej 4

---
+++ TA20Ej4_AdrianGallego/src/main/java/TA20Ej4_AdrianGallego/TA20Ej4_AdrianGallego/App.java
@@ -0,0 +1,9 @@
+package TA20Ej4_AdrianGallego.TA20Ej4_AdrianGallego;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		vistaEj4 window = new vistaEj4();
+		window.frame.setVisible(true);
+	}
+}
---
+++ TA20Ej4_AdrianGallego/src/main/java/TA20Ej4_AdrianGallego/TA20Ej4_AdrianGallego/vistaEj4.java
@@ -0,0 +1,36 @@
+package TA20Ej4_AdrianGallego.TA20Ej4_AdrianGallego;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class vistaEj4 {
+
+	public JFrame frame;
+
+	public vistaEj4() {
+		initialize();
+	}
+
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 600, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setLayout(null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setFont(new Font("Monospaced", Font.PLAIN, 14));
+		textArea.setBounds(106, 49, 431, 255);
+		frame.getContentPane().add(textArea);
+		
+		JLabel lblNewLabel = new JLabel("Eventos");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(21, 157, 70, 20);
+		frame.getContentPane().add(lblNewLabel);
+
+	}
+}
